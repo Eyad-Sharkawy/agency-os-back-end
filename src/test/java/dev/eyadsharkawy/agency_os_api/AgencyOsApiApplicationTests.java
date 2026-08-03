07545dbc54da345a9de4798775a9ce4b92deb657
Eyad-Sharkawy <eyad.m.sharkawy@gmail.com>
2026-08-03 14:15:44 +0300

    initial commit

---
+++ src/test/java/dev/eyadsharkawy/agency_os_api/AgencyOsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.eyadsharkawy.agency_os_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgencyOsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
